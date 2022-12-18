fb2d17443bede40b25a456b0068062d67754ce0b
Max Base <maxbasecode@gmail.com>
2022-12-18 23:36:38 +0100

    update

--- script.js
+++ script.js
@@ -18,21 +18,54 @@ let params = {
     ready_to_service_clients_in_queue: 0,
     queue_size: 0,
     max_simulation_clients: 0,
-    time_to_enter_queue_first_client: 0,
     min_time_to_enter_queue: 0,
     max_time_to_enter_queue: 0,
 };
+let clients = [];
 
 // Elements
 const elm_min_client_response_time = document.querySelector("#min_client_response_time");
-const elm_max_client_response_time= document.querySelector("#max_client_response_time");
-const elm_ready_to_service_clients_in_queue= document.querySelector("#ready_to_service_clients_in_queue");
+const elm_max_client_response_time = document.querySelector("#max_client_response_time");
+const elm_ready_to_service_clients_in_queue = document.querySelector("#ready_to_service_clients_in_queue");
 const elm_queue_size = document.querySelector("#queue_size");
-const elm_max_simulation_clients= document.querySelector("#max_simulation_clients");
-const elm_time_to_enter_queue_first_client= document.querySelector("#time_to_enter_queue_first_client");
-const elm_min_time_to_enter_queue= document.querySelector("#min_time_to_enter_queue");
-const elm_max_time_to_enter_queue= document.querySelector("#max_time_to_enter_queue");
+const elm_max_simulation_clients = document.querySelector("#max_simulation_clients");
+const elm_min_time_to_enter_queue = document.querySelector("#min_time_to_enter_queue");
+const elm_max_time_to_enter_queue = document.querySelector("#max_time_to_enter_queue");
 const elm_start_simulation = document.querySelector("#start_simulation");
+
+// Functions
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function simulation() {
+    // Create `elm_max_simulation_clients` clients
+    for (let i = 1; i <= params.max_simulation_clients; i++) {
+        let client = {
+            id: i,
+            time: random(params.min_client_response_time, params.max_client_response_time),
+            status: "in_stage",
+        };
+        clients.push(client);
+    }
+
+    // Create `elm_ready_to_service_clients_in_queue` clients
+    for (let i = 1; i <= params.ready_to_service_clients_in_queue; i++) {
+        clients[i - 1]["status"] = "in_queue";
+    }
+
+    // Start animation
+    let time = 0;
+    let interval = setInterval(() => {
+        time++;
+        console.log("Time: " + time);
+
+        // Check clients
+        for (let i = 0; i < clients.length; i++) {
+            
+        }
+    }, 1 * 1000);
+}
 
 // Events
 elm_start_simulation.addEventListener("click", () => {
@@ -46,11 +79,17 @@ elm_start_simulation.addEventListener("click", () => {
         ready_to_service_clients_in_queue: parseInt(elm_ready_to_service_clients_in_queue.value),
         queue_size: parseInt(elm_queue_size.value),
         max_simulation_clients: parseInt(elm_max_simulation_clients.value),
-        time_to_enter_queue_first_client: parseInt(elm_time_to_enter_queue_first_client.value),
         min_time_to_enter_queue: parseInt(elm_min_time_to_enter_queue.value),
         max_time_to_enter_queue: parseInt(elm_max_time_to_enter_queue.value),
     };
 
+    if (params.ready_to_service_clients_in_queue > params.elm_queue_size) {
+        alert("تعداد افراد آماده برای خدمت در صف بیشتر از اندازه صف است!");
+        return;
+    }
+
     state = "start";
     console.log(params);
+
+    simulation();
 });
